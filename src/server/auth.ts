319b3d4a9cbf927b8c652b184ad6cb75a147e2b5
Krish120003 <krish120003@gmail.com>
2023-09-16 03:54:10 -0400

    fix: remove discord provider

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -48,10 +48,6 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(db),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
     /**
      * ...add more providers here.
      *
