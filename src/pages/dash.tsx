8ca20bdc62d7a83289768387802adbc84261d422
Krish120003 <krish120003@gmail.com>
2023-09-16 06:20:17 -0400

    fix: disable ssr on dynamic import, fix color

--- src/pages/dash.tsx
+++ src/pages/dash.tsx
@@ -2,12 +2,13 @@ import dynamic from "next/dynamic";
 
 const DynamicDash = dynamic(() => import("../components/Dashboard"), {
   loading: () => <p>Loading...</p>,
+  ssr: false,
 });
 
 export default function Dash() {
   return (
     <>
-      <main className="min-h-screen w-full bg-yellow-400">
+      <main className="min-h-screen w-full bg-white">
         <DynamicDash />
       </main>
     </>
