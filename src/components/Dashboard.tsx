3b2cdd11a667331c21eece3acc389d587f9ea963
Krish120003 <krish120003@gmail.com>
2023-09-16 04:42:20 -0400

    chore: add react-grid-layout

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef } from "react";
+
+import GridLayout from "react-grid-layout";
+
+class MyFirstGrid extends React.Component {
+  render() {
+    // layout is an array of objects, see the demo for more complete usage
+    const layout = [
+      { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
+      { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+      { i: "c", x: 4, y: 0, w: 1, h: 2 },
+    ];
+    return (
+      <GridLayout
+        className="layout"
+        layout={layout}
+        cols={12}
+        rowHeight={30}
+        width={1200}
+      >
+        <div key="a">a</div>
+        <div key="b">b</div>
+        <div key="c">c</div>
+      </GridLayout>
+    );
+  }
+}
+
+const Dashboard = () => {
+  const gridRef = useRef(null);
+
+  return (
+    <div ref={gridRef}>
+      MyFirstGrid
+      <MyFirstGrid />
+    </div>
+  );
+};
+
+export default Dashboard;
