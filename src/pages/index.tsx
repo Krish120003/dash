f2def3e73ca66e9153e902842ba6dc48688308df
Rachelle DeMan <demanr@mcmaster.ca>
2023-09-17 03:01:33 -0400

    cleanup code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,6 @@ import { api } from "~/utils/api";
 import React from "react";
 import { GetServerSidePropsContext } from "next/types";
 import { getServerAuthSession } from "~/server/auth";
-import { ThemeProvider } from "~/components/theme-provider";
-import { ModeToggle } from "~/components/ui/toggle";
 
 export default function Home() {
   return (
@@ -26,12 +24,9 @@ export default function Home() {
       </Head>
 
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#401d6f] to-[#5559a5]">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <ModeToggle />
-          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 font-red-hat">
-            <Dash />
-          </div>
-        </ThemeProvider>
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 font-red-hat">
+          <Dash />
+        </div>
       </main>
     </>
   );
