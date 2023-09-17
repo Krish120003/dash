6b49e5099f7a37673552f55478b1d1cab080dedd
Arian Ahmadinejad <35879206+arian81@users.noreply.github.com>
2023-09-17 01:53:22 -0400

    fix: i don't even know

--- src/server/api/routers/location.ts
+++ src/server/api/routers/location.ts
@@ -1,38 +1,34 @@
-import { z } from "zod"
-import { createTRPCRouter, protectedProcedure } from "../trpc"
-import { db } from "~/server/db"
-import { LocationType } from "@prisma/client"
-
-
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { db } from "~/server/db";
+import { LocationType } from "@prisma/client";
 
 export const locationRouter = createTRPCRouter({
-    saveLocation: protectedProcedure.input(
-        z.object(
-            {
-                type: z.nativeEnum(LocationType),
-                coordinates: z.object(
-                    {
-                        latitude: z.number(),
-                        longitude: z.number()
-                    }
-                ),
-                radius: z.number()
-            }
-        )
-
-    ).mutation(async ({ input, ctx }) => {
-        await db.location.create({
-            data: {
-                type: input.type,
-                latitude: input.coordinates.latitude,
-                longitude: input.coordinates.longitude,
-                radius: input.radius,
-                user: {
-                    connect: {
-                        id: ctx.session.user.id
-                    }
-                }
-            }
-        })
-    })
-})
+  saveLocation: protectedProcedure
+    .input(
+      z.object({
+        type: z.nativeEnum(LocationType),
+        coordinates: z.object({
+          latitude: z.number(),
+          longitude: z.number(),
+        }),
+        radius: z.number(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      await db.location.create({
+        data: {
+          type: input.type,
+          latitude: input.coordinates.latitude,
+          longitude: input.coordinates.longitude,
+          radius: input.radius,
+          Layout: {
+            connect: {
+              id: ctx.session.user.id,
+            },
+          },
+          userId: ctx.session.user.id,
+        },
+      });
+    }),
+});
